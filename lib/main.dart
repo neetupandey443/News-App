e11e318a17806c61e57e8df4ffa1a0c059aeff8c
AllProject1 <e1062240001@timscdrmumbai.in>
2025-03-25 14:16:17 +0530

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'News App',
+      title: 'Samachar',
       theme: ThemeData(
         useMaterial3: true,
         fontFamily: GoogleFonts.poppins().fontFamily,
